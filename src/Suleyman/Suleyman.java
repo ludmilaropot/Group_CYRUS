b8fb872bda03c1ec511dfbb57dccc7c6c71f7f1f
syavuzer <yavuzercolorado@hotmail.com>
2022-12-28 12:07:07 -0700

    added another message

--- src/Suleyman/Suleyman.java
+++ src/Suleyman/Suleyman.java
@@ -1,4 +1,8 @@
 package Suleyman;
 
 public class Suleyman {
+    public static void main(String[] args) {
+        System.out.println("Group Cyrus is the BEST.");
+        System.out.println("We are the BEST");
+    }
 }
