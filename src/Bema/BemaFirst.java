fb61d167c3c70f4d81f2510fc1ef6ccb466bbc97
akbermetmuratova <Bemaibragimovna@gmail.com>
2022-12-29 22:04:20 -0600

    add Bema

--- src/Bema/BemaFirst.java
+++ src/Bema/BemaFirst.java
@@ -3,6 +3,6 @@ package Bema;
 public class BemaFirst {
 
     public static void main(String[] args) {
-        System.out.println("My first class");
+        System.out.println("My first class 1");
     }
 }
