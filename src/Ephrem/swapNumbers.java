5e05d94f91b8022ebd3eef414031f76ba80dba0c
emphi73 <emesfin73@yahoo.com>
2022-12-31 10:01:05 -0600

    Ephrem added new class called swapNumbers.

---
+++ src/Ephrem/swapNumbers.java
@@ -0,0 +1,12 @@
+package Ephrem;
+
+public class swapNumbers {
+    public static void main(String[] args) {
+        int x = 34;
+        int y = 78;
+        x = x+y;
+        y= x-y;
+        x = x-y;
+        System.out.println("x = "+x +"\ny = "+y);
+    }
+}
