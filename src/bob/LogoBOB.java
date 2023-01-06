b6073212c454780b8f9000c79904ae548e4568f9
Rikhsiboev <33729133+Rikhsiboev@users.noreply.github.com>
2023-01-05 19:13:27 -0700

    bob hi

---
+++ src/bob/LogoBOB.java
@@ -0,0 +1,7 @@
+package bob;
+
+public class LogoBOB {
+    public static void main(String[] args) {
+        System.out.println(" Logo Bob");
+    }
+}
---
+++ src/bob/MethodB.java
@@ -0,0 +1,12 @@
+package bob;
+
+public class MethodB extends methodA {
+
+        public void methodA(){
+            System.out.println("Method C");
+        }
+        public void methodB(){
+            System.out.println("Method B");
+        }
+    }
+
