6e0225daae72735eeb1120dcb78219b2c19499ab
Ludmila <ludmilaropot1@gmail.com>
2023-01-05 20:15:53 -0600

    Odd and Even

---
+++ src/Ludmila/Odd_Even.java
@@ -0,0 +1,27 @@
+package Ludmila;
+
+import java.util.Scanner;
+
+public class Odd_Even {
+    public static void main(String[] args) {
+            Scanner num=new Scanner(System.in);
+            System.out.println("Enter your number:");
+            int n=num.nextInt();
+            String number="";
+
+/*
+       if(n%2!=0){
+           number="Odd";
+       }else{
+           number="Even";
+       }
+        System.out.println("number "+n +" it's "+ number+" number");
+
+        System.out.println("------------------------------------------------");*/
+            String result=(n%2!=0)?"Odd":"Even";
+            System.out.println("result = " + result);
+
+
+
+        }
+}
